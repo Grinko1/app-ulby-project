c1b0e6f5b447054990d9056567b9f6083b325054
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-10-05 14:15:39 +0900

    fix sidebar

---
+++ src/widgets/Sidebar/model/selectors/getSideBarItems.ts
@@ -0,0 +1,37 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getUserAuthData } from 'entities/User';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
+import PostsIcon from 'shared/assets/icons/posts.svg';
+import { SidebarItemType } from '../types/sidebar';
+
+export const getSidebarItems = createSelector(getUserAuthData, (userData) => {
+  const sidebarItemsList: SidebarItemType[] = [
+    {
+      path: RoutePath.main,
+      Icon: MainIcon,
+      text: 'Главная',
+    },
+    {
+      path: RoutePath.about,
+      Icon: AboutIcon,
+      text: 'О сайте',
+    },
+    {
+      path: RoutePath.profile + userData?.id,
+      Icon: ProfileIcon,
+      text: 'Профиль',
+      authOnly: true,
+    },
+    {
+      path: RoutePath.articles,
+      Icon: PostsIcon,
+      text: 'Статьи',
+      authOnly: true,
+    },
+  ];
+  return sidebarItemsList
+});
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,8 +4,10 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
-import { SidebarItemsList } from '../../model/items';
+
 import { SidebarItem } from '../SidebarItem/SidebarItem';
+import { useSelector } from 'react-redux';
+import { getSidebarItems } from 'widgets/Sidebar/model/selectors/getSideBarItems';
 
 interface SidebarProps {
     className?: string;
@@ -14,6 +16,8 @@ interface SidebarProps {
 export const Sidebar = memo(({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
 
+    const SidebarItemsList = useSelector(getSidebarItems)
+    
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -25,7 +29,7 @@ export const Sidebar = memo(({ className }: SidebarProps) => {
             key={item.path}
 
         />
-    )), [collapsed]);
+    )), [collapsed, SidebarItemsList]);
 
     return (
         <div
